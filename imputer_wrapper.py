eb4cf64e38d67e8f8634b10d0cff255ea13fc949
Lin Yang(Search) <lnyang@linkedin.com>
2015-11-30 21:08:56 -0800

    x

--- imputer_wrapper.py
+++ imputer_wrapper.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python2.6
 
 from sklearn.preprocessing import Imputer
+import logging
 import numpy
 
 class ImputerWrapper:
   """ A simple wrapper around Imputer and supports using zero to fill in missing values.
+      If entire column is nan it gets filled with 0 to avoid Imputer removing the column.
   """
 
   def __init__(self, missing_values='NaN', strategy='zero', axis=0, verbose=0, copy=False):
@@ -13,9 +15,22 @@ class ImputerWrapper:
     if strategy != 'zero':
       self.imputer = Imputer(missing_values, strategy, axis, verbose, copy)
 
+  def prepare(self, X):
+    for j in range(X.shape[1]):
+      all_nan = True
+      for i in range(X.shape[0]):
+        if not numpy.isnan(X[i][j]):
+          all_nan = False
+          break
+      if all_nan:
+        logging.info('column %d all nan, filling with 0' % j)
+        for i in range(X.shape[0]):
+          X[i][j] = 0.0
+
   def fit(self, X, y=None):
     if self.strategy == 'zero':
       return self
+    self.prepare(X)
     self.imputer.fit(X, y)
     return self
 
@@ -26,6 +41,7 @@ class ImputerWrapper:
           if numpy.isnan(X[i][j]):
             X[i][j] = 0.0
       return X
+    self.prepare(X)
     return self.imputer.fit_transform(X, y, **fit_params)
 
   def get_params(self, deep=True):
